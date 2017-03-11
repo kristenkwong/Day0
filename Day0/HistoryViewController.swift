e30ba594bd266df8fe08e9761dadd367887318d8
Milton Leung <milleung964@gmail.com>
2017-03-11 14:33:10 -0800

    wip

---
+++ Day0/HistoryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryViewController.swift
+//  Day0
+//
+//  Created by Milton Leung on 2017-03-11.
+//  Copyright © 2017 0mbps. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+  
+  @IBOutlet weak var historyTextView: UITextView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    historyTextView.text = HistoryData.shared.getEmojiHistory()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
